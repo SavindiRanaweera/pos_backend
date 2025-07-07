4a2768a63fff83b105098c9dd9ede5d73147aaa7
Savindi Ranaweera <savindi.ch1995@gmail.com>
2025-07-07 10:48:20 +0530

    Initial commit

---
+++ src/test/java/com/pos/backend_pos/BackendPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pos.backend_pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
